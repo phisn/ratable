4ac7d7b53df4384bf69fde6992af96818f18df78
phisn <phisn@outlook.de>
2022-10-08 16:23:48 +0200

    Force resource finding from root

--- webapp/webpack.config.dev.js
+++ webapp/webpack.config.dev.js
@@ -1,12 +1,21 @@
 const {webDev} = require("@fun-stack/fun-pack");
 
 // https://github.com/fun-stack/fun-pack
-module.exports = webDev({
+const config = webDev({
   indexHtml: "src/main/html/index.html",
   extraWatchDirs: [
-    "assets/dev"
+    "assets"
   ],
   extraStaticDirs: [
     "src" // for source maps
   ]
 });
+
+// Enforcing resource finding from root directory to implement path based routing
+// https://github.com/fun-stack/fun-pack/blob/master/src/webpack.config.web.dev.js
+// https://webpack.js.org/plugins/html-webpack-plugin/
+// https://webpack.js.org/guides/public-path/
+// https://stackoverflow.com/questions/34620628/htmlwebpackplugin-injects-relative-path-files-which-breaks-when-loading-non-root
+config.output.publicPath = "/";
+
+module.exports = config;
