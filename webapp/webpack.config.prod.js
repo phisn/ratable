c0be914e27833a8d8bb80573d9a4640088165eb4
phisn <phisn@outlook.de>
2022-10-09 11:55:12 +0200

    Fix wrong asset location in prod

--- webapp/webpack.config.prod.js
+++ webapp/webpack.config.prod.js
@@ -1,7 +1,12 @@
 const {webProd} = require("@fun-stack/fun-pack");
 
 // https://github.com/fun-stack/fun-pack
-module.exports = webProd({
+const config = webProd({
   indexHtml: "src/main/html/index.html",
   assetsDir: "assets",
 });
+
+// See webpack.config.dev.js for explanation
+config.output.publicPath = "/";
+
+module.exports = config;
