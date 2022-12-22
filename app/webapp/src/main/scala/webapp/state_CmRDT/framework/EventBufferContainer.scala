b4c121ad6f77e8025d9ac03273b9fb4e7a84007c
phisn <phisn@outlook.de>
2022-12-22 03:16:04 +0100

    Implement EventBufferContainer as event buffering for new crdt

--- app/webapp/src/main/scala/webapp/state_CmRDT/framework/EventBufferContainer.scala
+++ app/webapp/src/main/scala/webapp/state_CmRDT/framework/EventBufferContainer.scala
@@ -1,15 +1,48 @@
 package webapp.state_CmRDT.framework
 
 import core.framework.ecmrdt.*
+import scala.concurrent.*
+import scala.concurrent.ExecutionContext.Implicits.global
 
 case class EventBufferContainer[A, C](
   val inner: ECmRDT[A, C],
   val events: Set[ECmRDTEventWrapper[A, C]]
 ):
-  def prepare(eventWithContext: EventWithContext[A, C])(using effectPipeline: EffectPipeline[A, C]) =
-    inner.prepare(eventWithContext)
+  def effect(eventPrepared: ECmRDTEventWrapper[A, C])(using EffectPipeline[A, C]): Future[Either[String, EventBufferContainer[A, C]]] =
+    inner.effect(eventPrepared)
+      .map(_.map(newInner =>
+        EventBufferContainer(
+          inner = newInner,
+          events = events + eventPrepared
+        )
+      ))
 
-  def effect(wrapper: ECmRDTEventWrapper[A, C])(using effectPipeline: EffectPipeline[A, C]) =
-    wrapper.
-    ???
+  def effect(event: EventWithContext[A, C])(using EffectPipeline[A, C]): Future[Either[String, EventBufferContainer[A, C]]] =
+    // Prepare event
+    inner.prepare(event)
+      .flatMap {
+        case Left(message) => 
+          Future.successful(Left(message))
 
+        // If successfull
+        case Right(prepared) => 
+          // Effect event
+          inner.effect(prepared)
+            // If successfull
+            .map(_.map(newInner =>
+              // Return new container
+              EventBufferContainer(
+                inner = newInner,
+                events = events + prepared
+              )
+            ))
+      }
+  
+  // Server responds with highest time to acknowledge events
+  def acknowledge(time: Long) =
+    EventBufferContainer(
+      inner = inner,
+      // All events with time higher than the acknowledged time remain
+      events = events.filter(_.time > time)
+    )
+      
